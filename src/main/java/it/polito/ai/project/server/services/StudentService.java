51f4bd621fa77b68a21612a172562699cc6d14a8
Eugenio <eugeniocorso95@gmail.com>
2020-11-12 19:02:41 +0100

    added changeVirtualMachineParameters in studentService

--- src/main/java/it/polito/ai/project/server/services/StudentService.java
+++ src/main/java/it/polito/ai/project/server/services/StudentService.java
@@ -1,17 +1,15 @@
 package it.polito.ai.project.server.services;
 
-
-import it.polito.ai.project.server.dtos.TeamDTO;
 import it.polito.ai.project.server.dtos.VirtualMachineDTO;
 
 import java.util.List;
 
 public interface StudentService {
-    void createVirtualMachine(Integer cpu, Integer ram, Integer diskspace,
-                              String vmName, Long teamId, String owner);
+    void createVirtualMachine(VirtualMachineDTO virtualMachineDTO, Long teamId, String owner);
+    void changeVirtualMachineParameters(VirtualMachineDTO virtualMachineDTO, String owner);
     void startVirtualMachine(Long vmId, String studentId);
     void stopVirtualMachine(Long vmId, String studentId);
     void deleteVirtualMachine(Long vmId, String studentId);
     void addVirtualMachineOwners(String owner, List<String> students, Long vmId);
-    VirtualMachineDTO getVirtualMachine(String studentId, Long vmId);
+    VirtualMachineDTO getVirtualMachine(Long vmId);
 }
--- src/main/java/it/polito/ai/project/server/services/StudentServiceImpl.java
+++ src/main/java/it/polito/ai/project/server/services/StudentServiceImpl.java
@@ -39,17 +39,13 @@ public class StudentServiceImpl implements StudentService{
 
     /**
      * Function to create a virtual machine for a given team
-     * @param cpu virtual machine cpu
-     * @param ram virtual machine ram
-     * @param diskspace virtual machine disk space
-     * @param vmName virtual machine name given by the creator
+     * @param virtualMachineDTO virtual machine DTO
      * @param teamId team to which the virtual machine belongs
      * @param owner list of the virtual machine owers
      */
     @PreAuthorize("hasRole('ROLE_STUDENT')")
     @Override
-    public void createVirtualMachine(Integer cpu, Integer ram, Integer diskspace,
-                                                   String vmName, Long teamId, String owner) {
+    public void createVirtualMachine(VirtualMachineDTO virtualMachineDTO, Long teamId, String owner) {
 
         Optional<Team> teamOptional = this.teamRepository.findById(teamId);
         Optional<Student> studentOptional = this.studentRepository.findById(owner);
@@ -98,7 +94,7 @@ public class StudentServiceImpl implements StudentService{
         if(teamOptional.get().getVirtualMachines().stream()
                                     .map(VirtualMachine::getName)
                                     .collect(Collectors.toList())
-                                    .contains(vmName)
+                                    .contains(virtualMachineDTO.getName())
         ){
             throw new StudentServiceException("The vm name must be unique");
 
@@ -108,30 +104,33 @@ public class StudentServiceImpl implements StudentService{
         if((teamOptional.get().getVirtualMachines()
                 .stream()
                 .map(VirtualMachine::getCpu)
-                .reduce(0, (a, b) -> a+b) + cpu) > teamOptional.get().getCpuMax()){
+                .reduce(0, (a, b) -> a+b) + virtualMachineDTO.getCpu()) > teamOptional.get().getCpuMax()){
             throw new StudentServiceException("Cpu limit exceeded");
         }
 
         if((teamOptional.get().getVirtualMachines()
                 .stream()
                 .map(VirtualMachine::getRam)
-                .reduce(0, (a, b) -> a+b) + ram) > teamOptional.get().getRamMax()){
+                .reduce(0, (a, b) -> a+b) + virtualMachineDTO.getRam()) > teamOptional.get().getRamMax()){
             throw new StudentServiceException("RAM limit exceeded");
         }
 
         if((teamOptional.get().getVirtualMachines()
                 .stream()
                 .map(VirtualMachine::getDiskSpace)
-                .reduce(0, (a, b) -> a+b) + diskspace)> teamOptional.get().getDiskSpaceMax()){
+                .reduce(0, (a, b) -> a+b) + virtualMachineDTO.getDiskSpace())
+                                            > teamOptional.get().getDiskSpaceMax()
+        )
+        {
             throw new StudentServiceException("Disk space limit exceeded");
         }
 
         // create the virtual machine
         virtualMachine = VirtualMachine.builder()
-                                        .name(vmName)
-                                        .cpu(cpu)
-                                        .diskSpace(diskspace)
-                                        .ram(ram)
+                                        .name(virtualMachineDTO.getName())
+                                        .cpu(virtualMachineDTO.getCpu())
+                                        .diskSpace(virtualMachineDTO.getDiskSpace())
+                                        .ram(virtualMachineDTO.getRam())
                                         .active(false)
                                         .build();
 
@@ -140,6 +139,78 @@ public class StudentServiceImpl implements StudentService{
 
         this.virtualMachinesRepository.save(virtualMachine);
     }
+
+
+    /**
+     * Change virtual machine used resources
+     * @param virtualMachineDTO the DTO with the new resources
+     * @param owner one of the owners of the virtual machine
+     */
+    public void changeVirtualMachineParameters(VirtualMachineDTO virtualMachineDTO, String owner){
+        Optional<VirtualMachine> virtualMachineOptional = this.virtualMachinesRepository
+                                                                .findById(virtualMachineDTO.getId());
+        Optional<Student> studentOptional = this.studentRepository.findById(owner);
+        Team team;
+
+        // check if the vm exists
+        if (!virtualMachineOptional.isPresent()) {
+            throw new StudentServiceException("Virtual machine doesn't exists");
+        }
+
+        // check if the student exists
+        if (!studentOptional.isPresent()) {
+            throw new StudentNotFoundExeption();
+        }
+
+        // check if the student is the owner
+        if (!virtualMachineOptional.get().getOwners().contains(studentOptional.get())) {
+            throw new StudentServiceException("The student is not one of the owner of the virtual machine");
+        }
+
+        team = virtualMachineOptional.get().getTeam();
+
+        // check if the team is active
+        if(team.getStatus() != 1){
+            throw new StudentServiceException("Team not active");
+        }
+
+        // check if the course is enabled
+        if(!team.getCourse().isEnabled()){
+            throw new StudentServiceException("Course not enabled");
+        }
+
+        // check the cpu, ram and diskspace constraints
+        if (
+            (team.getVirtualMachines().stream().map(VirtualMachine::getCpu).reduce(0, Integer::sum)
+                + virtualMachineDTO.getCpu() - virtualMachineOptional.get().getCpu())
+            > team.getCpuMax()
+        ) {
+            throw new StudentServiceException("Cpu limit exceeded");
+        }
+
+        if (
+            (team.getVirtualMachines().stream().map(VirtualMachine::getRam).reduce(0, Integer::sum)
+                    + virtualMachineDTO.getRam() - virtualMachineOptional.get().getRam())
+            > team.getRamMax()
+        ) {
+            throw new StudentServiceException("RAM limit exceeded");
+        }
+
+        if (
+            (team.getVirtualMachines().stream().map(VirtualMachine::getDiskSpace).reduce(0, Integer::sum)
+                + virtualMachineDTO.getDiskSpace() - virtualMachineOptional.get().getDiskSpace())
+             > team.getDiskSpaceMax()
+        ) {
+            throw new StudentServiceException("Disk space limit exceeded");
+        }
+
+        virtualMachineOptional.get().setCpu(virtualMachineDTO.getCpu());
+        virtualMachineOptional.get().setRam(virtualMachineDTO.getRam());
+        virtualMachineOptional.get().setDiskSpace(virtualMachineDTO.getDiskSpace());
+
+        this.virtualMachinesRepository.save(virtualMachineOptional.get());
+    }
+
 
     /**
      * Funcition to start an existing Virtual Machine
@@ -164,6 +235,11 @@ public class StudentServiceImpl implements StudentService{
         // check if the course is enabled
         if(!virtualMachineOptional.get().getTeam().getCourse().isEnabled()) {
             throw new StudentServiceException("Course not enabled");
+        }
+
+        // check if the team is active
+        if(virtualMachineOptional.get().getTeam().getStatus() != 1){
+            throw new StudentServiceException("Team not active");
         }
 
         // check if the student is one of the owner of the virtual machine
@@ -204,6 +280,11 @@ public class StudentServiceImpl implements StudentService{
             throw new StudentServiceException("Course not enabled");
         }
 
+        // check if the team is active
+        if(virtualMachineOptional.get().getTeam().getStatus() != 1){
+            throw new StudentServiceException("Team not active");
+        }
+
         // check if the student is one of the owner of the virtual machine
         if (virtualMachineOptional.get().getOwners()
                 .stream()
@@ -237,6 +318,11 @@ public class StudentServiceImpl implements StudentService{
             throw new StudentNotFoundExeption();
         }
 
+        // check if the team is active
+        if(virtualMachineOptional.get().getTeam().getStatus() != 1){
+            throw new StudentServiceException("Team not active");
+        }
+
         // check if the student is one of the owner of the virtual machine
         if (!virtualMachineOptional.get().getOwners()
                 .stream()
@@ -248,10 +334,10 @@ public class StudentServiceImpl implements StudentService{
         }
 
         // delete the virtual machine from all the students and the team
-        virtualMachineOptional.get()
-                            .getOwners()
+        virtualMachineOptional.get().getOwners()
                             .forEach(x -> virtualMachineOptional.get().removeOwner(x));
         virtualMachineOptional.get().setTeam(null);
+
         this.virtualMachinesRepository.deleteById(vmId);
     }
 
@@ -298,6 +384,11 @@ public class StudentServiceImpl implements StudentService{
             throw new StudentServiceException("Course not enabled");
         }
 
+        // check if the team is active
+        if(virtualMachineOptional.get().getTeam().getStatus() != 1){
+            throw new StudentServiceException("Team not active");
+        }
+
         // add all students as owners of the virtual machine
         students.stream()
                 .map(x -> this.studentRepository.findById(x).get())
@@ -308,18 +399,11 @@ public class StudentServiceImpl implements StudentService{
 
     /**
      * Get an existing virtual machine
-     * @param studentId the student id
      * @param vmId the virtual machine id
      * @return the virtual machine DTO
      */
-    public VirtualMachineDTO getVirtualMachine(String studentId, Long vmId){
+    public VirtualMachineDTO getVirtualMachine(Long vmId){
         Optional<VirtualMachine> virtualMachineOptional = this.virtualMachinesRepository.findById(vmId);
-        Optional<Student> studentOptional = this.studentRepository.findById(studentId);
-
-        // check if the student exists
-        if(!studentOptional.isPresent()){
-            throw new StudentNotFoundExeption();
-        }
 
         // check if the vm exists
         if(!virtualMachineOptional.isPresent()){
@@ -329,6 +413,11 @@ public class StudentServiceImpl implements StudentService{
         // check if the course is enabled
         if(!virtualMachineOptional.get().getTeam().getCourse().isEnabled()){
             throw new StudentServiceException("Course not enabled");
+        }
+
+        // check if the team is active
+        if(virtualMachineOptional.get().getTeam().getStatus() != 1){
+            throw new StudentServiceException("Team not active");
         }
 
         return this.modelMapper.map(virtualMachineOptional.get(),
