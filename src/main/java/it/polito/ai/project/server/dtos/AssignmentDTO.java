c8135ae723ea14d9c19b6dbbb4ac2635945929c3
Eugenio <eugeniocorso95@gmail.com>
2020-11-15 19:41:24 +0100

    fixed assignment dto

--- src/main/java/it/polito/ai/project/server/dtos/AssignmentDTO.java
+++ src/main/java/it/polito/ai/project/server/dtos/AssignmentDTO.java
@@ -3,7 +3,7 @@ package it.polito.ai.project.server.dtos;
 import lombok.Data;
 import org.springframework.hateoas.RepresentationModel;
 
-import java.util.Date;
+import java.sql.Date;
 
 @Data
 public class AssignmentDTO extends RepresentationModel<AssignmentDTO> {
