429f746af7537cb2c58e6bbd063985bdbbbbde48
Gabriele <gabrielecorso95@gmail.com>
2020-12-29 12:49:36 +0100

    removed preauthorize in getTeamsForCourse in TeamServiceImpl

--- src/main/java/it/polito/ai/project/server/services/TeamServiceImpl.java
+++ src/main/java/it/polito/ai/project/server/services/TeamServiceImpl.java
@@ -67,7 +67,6 @@ public class TeamServiceImpl implements TeamService {
      * @param courseName the course name
      * @return list of all the teams in the course
      */
-    @PreAuthorize("hasRole('ROLE_TEACHER')")
     @Override
     public List<TeamDTO> getTeamsForCourse(String courseName) {
         Optional<Course> courseOptional = courseRepository.findById(courseName);
