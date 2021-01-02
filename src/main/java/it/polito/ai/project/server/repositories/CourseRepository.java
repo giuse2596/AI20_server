dc79884775bd4447479a314668f325470945165f
Eugenio <eugeniocorso95@gmail.com>
2021-01-02 23:07:48 +0100

    fixed getStudentsNotInTeams in courseRepository

--- src/main/java/it/polito/ai/project/server/repositories/CourseRepository.java
+++ src/main/java/it/polito/ai/project/server/repositories/CourseRepository.java
@@ -14,7 +14,7 @@ public interface CourseRepository extends JpaRepository<Course,String> {
     @Query("SELECT s FROM Student s INNER JOIN s.teams t INNER JOIN t.course c WHERE c.name=:courseName AND t.active=true")
     List<Student> getStudentsInTeams(String courseName);
 
-    @Query("SELECT s FROM Student s INNER JOIN s.courses c WHERE s NOT IN " +
+    @Query("SELECT s FROM Student s INNER JOIN s.courses c WHERE c.name=:courseName AND s NOT IN " +
             "(SELECT s FROM Student s INNER JOIN s.teams t INNER JOIN t.course c WHERE c.name=:courseName AND t.active=true)")
     List<Student> getStudentsNotInTeams(String courseName);
 }
